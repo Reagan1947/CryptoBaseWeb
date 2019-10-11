f6edf17dbc0d8f4d71149bf0293c0dacfa34dbd0
Reaganlee <buwang1947@live.com>
2019-10-11 14:00:11 +0800

    add login file

--- CryptoBaseWeb/registration_center.py
+++ CryptoBaseWeb/registration_center.py
@@ -1,7 +1,6 @@
 # -*-coding:utf8-*-
 import random
 from CryptoBaseWeb.abenc_bsw07 import *
-import sqlite3
 from base.models import *
 
 
@@ -22,62 +21,112 @@ class RegistrationCenter:
         h = self.groupObj.serialize(self.pk['h'])
         f = self.groupObj.serialize(self.pk['f'])
         e_gg_alpha = self.groupObj.serialize(self.pk['e_gg_alpha'])
-        PPK = {'g': g, 'g2': g2, 'h': h, 'f': f, 'e_gg_alpha': e_gg_alpha}
+        self.PPK = {'g': g, 'g2': g2, 'h': h, 'f': f, 'e_gg_alpha': e_gg_alpha}
 
-        BaseInformation.objects.create(service_id=1, service_key=service_key_01, tpk=str(PPK), mk=MK)
-        BaseInformation2.objects.create(service_id=2, service_key=service_key_02, tpk=str(PPK), mk=MK)
+        BaseInformation.objects.create(service_id=1, service_key=service_key_01, tpk=str(self.PPK), mk=MK)
+        BaseInformation2.objects.create(service_id=2, service_key=service_key_02, tpk=str(self.PPK), mk=MK)
 
     def smart_car(self, user_id, mNBPW):
         global service_key_01, service_key_02
         pair_key_01 = random.getrandbits(1024)
         pair_key_02 = random.getrandbits(1024)
-        conn_01 = sqlite3.connect('service_01.db')
-        conn_02 = sqlite3.connect('service_02.db')
-        c_01 = conn_01.cursor()
-        c_02 = conn_02.cursor()
-        cursor_01 = c_01.execute("SELECT service_key  from main.base_information")
-        for row_01 in cursor_01:
-            service_key_01 = row_01[0]
-        cursor_02 = c_02.execute("SELECT service_key  from main.base_information")
-        for row_02 in cursor_02:
-            service_key_02 = row_02[0]
+        # conn_01 = sqlite3.connect('service_01.db')
+        # conn_02 = sqlite3.connect('service_02.db')
+        # c_01 = conn_01.cursor()
+        # c_02 = conn_02.cursor()
+        # cursor_01 = c_01.execute("SELECT service_key  from main.base_information")
+        # for row_01 in cursor_01:
+        #     service_key_01 = row_01[0]
+        # cursor_02 = c_02.execute("SELECT service_key  from main.base_information")
+        # for row_02 in cursor_02:
+        #     service_key_02 = row_02[0]
 
-        c_01.execute("insert into main.authentica_information(user_id, user_fake_id, pair_key) "
-                     "values (\"{}\", \"{}\", \"{}\")".format(user_id, user_id + 200, str(pair_key_01)))
-        c_02.execute("insert into main.authentica_information(user_id, user_fake_id, pair_key) "
-                     "values (\"{}\", \"{}\", \"{}\")".format(user_id, user_id + 200, str(pair_key_02)))
+        # c_01.execute("insert into main.authentica_information(user_id, user_fake_id, pair_key) "
+        #              "values (\"{}\", \"{}\", \"{}\")".format(user_id, user_id + 200, str(pair_key_01)))
+        # c_02.execute("insert into main.authentica_information(user_id, user_fake_id, pair_key) "
+        #              "values (\"{}\", \"{}\", \"{}\")".format(user_id, user_id + 200, str(pair_key_02)))
+        #
+        # conn_01.commit()
+        # conn_02.commit()
+        # conn_01.close()
+        # conn_02.close()
 
-        conn_01.commit()
-        conn_02.commit()
-        conn_01.close()
-        conn_02.close()
+        service_key_01 = BaseInformation.objects.get(service_id=1)
+        service_key_01 = service_key_01.service_key
+        service_key_02 = BaseInformation2.objects.get(service_id=2)
+        service_key_02 = service_key_02.service_key
+        user_fake_id = int(user_id) + 200
+        Authentic.objects.create(user_id=int(user_id), user_fake_id=user_fake_id, pair_key=str(pair_key_01))
+        Authentic2.objects.create(user_id=int(user_id), user_fake_id=user_fake_id, pair_key=str(pair_key_02))
+
         M_01 = hash(str(hash(int(pair_key_01) ^ int(user_id))) + str(service_key_01))
-        M_02 = hash(str(hash(pair_key_02 ^ user_id)) + str(service_key_02))
+        M_02 = hash(str(hash(int(pair_key_02) ^ int(user_id))) + str(service_key_02))
         N_01 = M_01 ^ mNBPW
         N_02 = M_02 ^ mNBPW
         NId_cs_01 = hash(str(1) + service_key_01)
         NId_cs_02 = hash(str(2) + service_key_02)
         # user temple_id ##############################################
-        temple_user_id = user_id + 200
+        temple_user_id = int(user_id) + 200
         #
         attrs_01 = ['ONE', 'TWO', 'THREE']
         attrs_02 = ['ONE', 'TWO', 'THREE']
         sk_01 = self.cpabe.keygen(self.pk, self.mk, attrs_01)
         sk_02 = self.cpabe.keygen(self.pk, self.mk, attrs_02)
+
+        sk_01 = RegistrationCenter.serialization(self, sk_01)
+        sk_02 = RegistrationCenter.serialization(self, sk_02)
+
         service_result_01 = [1, N_01, NId_cs_01, attrs_01, sk_01]
         service_result_02 = [2, N_02, NId_cs_02, attrs_02, sk_02]
         sr = [service_result_01, service_result_02]
-        smart_car_result = [temple_user_id, sr, self.pk]
+        smart_car_result = [temple_user_id, sr, self.PPK]
 
         return smart_car_result
 
         # base information
         # service id, service_key , master_key, master_public_key
 
-    def sezerlize(self, aim_dict):
-        for index in aim_dict:
-            temple = aim_dict[index]
-            print(type(temple))
-            aim_dict[index] = self.groupObj.serialize(temple)
-        print(aim_dict)
-        return aim_dict
+    # def sezerlize(self, aim_dict):
+    #     for index in aim_dict:
+    #         temple = aim_dict[index]
+    #         print(type(temple))
+    #         aim_dict[index] = self.groupObj.serialize(temple)
+    #     print(aim_dict)
+    #     return aim_dict
+
+    def serialization(self, original_sk):
+        # groupObj = PairingGroup('SS512')  # temple
+
+        D = original_sk['D']
+        Dj = original_sk['Dj']
+        Djp = original_sk['Djp']
+        S = original_sk['S']
+
+        # serialization D
+        D = self.groupObj.serialize(D)
+
+        # serialization Dj
+        Dj_ONE = Dj['ONE']
+        Dj_TWO = Dj['TWO']
+        Dj_THREE = Dj['THREE']
+
+        Dj_ONE = self.groupObj.serialize(Dj_ONE)
+        Dj_TWO = self.groupObj.serialize(Dj_TWO)
+        Dj_THREE = self.groupObj.serialize(Dj_THREE)
+
+        Dj = {'ONE': Dj_ONE, 'TWO': Dj_TWO, 'THREE': Dj_THREE}
+
+        # serialization Djp
+        Djp_ONE = Djp['ONE']
+        Djp_TWO = Djp['TWO']
+        Djp_THREE = Djp['THREE']
+
+        Djp_ONE = self.groupObj.serialize(Djp_ONE)
+        Djp_TWO = self.groupObj.serialize(Djp_TWO)
+        Djp_THREE = self.groupObj.serialize(Djp_THREE)
+
+        Djp = {'ONE': Djp_ONE, 'TWO': Djp_TWO, 'THREE': Djp_THREE}
+
+        # s is list
+
+        return {'D': D, 'Dj': Dj, 'Djp': Djp, 'S': S}
--- base/views.py
+++ base/views.py
@@ -1,28 +1,54 @@
 from django.shortcuts import render
 from CryptoBaseWeb.registration_center import *
+from CryptoBaseWeb.mobile_user_register import *
+from CryptoBaseWeb.mobile_user_login import *
+from CryptoBaseWeb.cloud_service_provider import *
+import pickle
 
 
+# change the object register to register_init
 # Create your views here.
-def register(request):
 
+def register(request):
     return render(request, 'register.html')
 
 
 def login(request):
+    mobile_login = MobileUserLogin(12, '123')
+    log_in_result = mobile_login.log_in(smart_car, 1)
+
+    cloudProvider = CloudServiceProvider()
+    PK = smart_car[9]
+    au_result = cloudProvider.authentication(log_in_result, 1, register, PK)
+    mobile_login.monile_authentiation(au_result, smart_car, 1)
     return None
 
 
 def init(request):
-    register = RegistrationCenter()
-    register.initation()
+    global register_init
+    register_init = RegistrationCenter()
+    register_init.initation()
+
     return render(request, 'register.html')
 
 
 def do_register(request):
+    user_id = ''
+    password = ''
+
     if request.method == "POST":
         user_id = request.POST.get('user_id')
         password = request.POST.get('password')
 
-        print(user_id)
-        print(password)
-    return None
+    mobile_register = MobileUserRegister(user_id, str(password))
+    NBPW_result = mobile_register.NBPW_gen()
+    user_id = NBPW_result[0]
+    NBPW = NBPW_result[1]
+    smart_car_result = register_init.smart_car(user_id, NBPW)
+    smart_car = mobile_register.smart_car_save(smart_car_result)
+
+    data_name_str = 'data' + user_id + '.pkl'
+    output = open(data_name_str, 'wb')
+    pickle.dump(smart_car, output)
+
+    return render(request, 'smart_car.html')
